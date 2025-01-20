8f0974d94294a7ebb04d30ff5f19daf41d2bc8d0
Dheeraj-G <gosuladheeraj@gmail.com>
2025-01-20 18:06:10 -0500

    env variables

--- website/my-app/src/app/components/contact/submit-form.tsx
+++ website/my-app/src/app/components/contact/submit-form.tsx
@@ -6,14 +6,14 @@ import { z } from "zod";
 import { formSchema } from "./schemas";
 import React from 'react';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 
 export const send = async (emailFormData: z.infer<typeof formSchema>) => {
   try {
     // TODO: Add this emailFormData to some database
 
     const { error } = await resend.emails.send({
-      from: `${emailFormData.firstName} ${emailFormData.lastName} <${process.env.RESEND_FROM_EMAIL}>`,
+      from: `${emailFormData.firstName} ${emailFormData.lastName} <${process.env.NEXT_PUBLIC_RESEND_FROM_EMAIL}>`,
       to: "gosuladheeraj@gmail.com",
       subject: "Email Form Submission",
       react: React.createElement(EmailTemplate, {email: emailFormData.email, message: emailFormData.message }),
